5d86772ccca49480def98cd0d73028d0eb7de802
Lucas Mayer <lkmayer410@gmail.com>
2021-12-08 13:23:52 -0300

    Lucas Mayer: Criação do Package Dao, classes TarefaDao e UsuárioDao

---
+++ Projetojavaback/src/main/java/com/ifrs/edu/Projetojavaback/Dao/UsuarioDAO.java
@@ -0,0 +1,11 @@
+package com.ifrs.edu.Projetojavaback.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ifrs.edu.Projetojavaback.Entidades.Usuario;
+
+@Repository
+public interface UsuarioDAO extends CrudRepository <Usuario, Integer> {
+
+}
