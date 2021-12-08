c0cbe49b12c32044e8d748c0ee0c425557802d57
Lucas Mayer <lkmayer410@gmail.com>
2021-12-08 16:03:43 -0300

    Lucas: Faz a inserção do usuário e verifica se ele já não está cadastrado

--- Projetojavaback/src/main/java/com/ifrs/edu/Projetojavaback/Controller/Usuarios.java
+++ Projetojavaback/src/main/java/com/ifrs/edu/Projetojavaback/Controller/Usuarios.java
@@ -25,14 +25,14 @@ public class Usuarios {
 
 /////// VALIDAÇão DO CADASTRO DO USUÁRIO//////////
 
-    public void validaUsuario(Usuario usuario){
-        if (usuario.getNomeUsuario() == null || usuario.getNomeUsuario().isEmpty()){
+    public void validaUsuario(Usuario usuario) {
+        if (usuario.getNomeUsuario() == null || usuario.getNomeUsuario().isEmpty()) {
             throw new RequisicaoInvalida("Usuário tem que ter nome");
         }
-             if (usuario.getTelefoneusuario()== null || usuario.getTelefoneusuario().isEmpty()){
+        if (usuario.getTelefoneusuario() == null || usuario.getTelefoneusuario().isEmpty()) {
             throw new RequisicaoInvalida("Usuário tem que ter telefone");
         }
-        if (usuario.getEmailusuario() == null || usuario.getEmailusuario().isEmpty()){
+        if (usuario.getEmailusuario() == null || usuario.getEmailusuario().isEmpty()) {
             throw new RequisicaoInvalida("Usuário tem que ter e-mail");
         }
     }
@@ -44,3 +44,37 @@ public class Usuarios {
     public Iterable<Usuario> listar() {
         return usuarioDao.findAll();
     }
+
+    ///// FAz a inserção do do usuário e verifica se o usuário já foi cadastrado ///
+
+    @RequestMapping(path = "/usuarios/", method = RequestMethod.POST)
+    public Usuario inserir(@RequestBody Usuario usuario) {
+        usuario.setId(0);
+        validaUsuario(usuario);
+        Iterable<Usuario> usuarios = usuarioDao.findAll();
+        boolean nomeIgual = false;
+        for (Usuario nomeEntrada : usuarios) {
+            if (nomeEntrada.getNomeUsuario().equals(usuario.getNomeUsuario())) {
+                nomeIgual = true;
+                break;
+            }
+        }
+        if (nomeIgual == true) {
+            throw new RequisicaoInvalida("Este usuário já foi cadastrado");
+
+        }
+        return usuarioDao.save(usuario);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
